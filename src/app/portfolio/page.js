463a363490a6db08c9cb6b4cd67c42f0ee49cafb
Stephany Munoz <stephany.munoz8@gmail.com>
2024-06-27 19:21:29 -0400

    trying to do portfolio page

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,39 +1,53 @@
+import Image from "next/image.js";
+import Link from "next/link.js";
 import Navbar from "../../components/Navbar.js";
 import styles from "../../styles/Portfolio.module.css";
-
+import Writing from "../../../public/cursive-writing-ozzie-gonzalez.jpg";
+import Spot from "../../../public/the-spot-ozzie-gonzalez.jpg";
+import Vine from "../../../public/on-a-vine.jpg";
+import Days from "../../../public/days-end-ozzie-gonzalez.jpg";
 const DesktopPortfolioCategories = () => {
   return (
     <div className={styles.desktopPortfolioCategories}>
       <header className={styles.headerWrapper}>
-        <img
-          className={styles.headerIcon}
-          loading="lazy"
-          alt=""
-          src="/signature-dark.png"
-        />
+        <img className={styles.headerIcon} loading="lazy" alt="" src="/signature-dark.png" />
       </header>
       <Navbar />
       <div className={styles.imageTextWrapper}>
-        <img
-          className={styles.imageIcon}
-          alt=""
-          src="/cursivewritingozziegonzalez-1@2x.png"
-        />
-        <div className={styles.textWrapper}>{`Landscape Portfolio `}</div>
+        <Image className={styles.imageIcon} alt="" src={Writing} width={400} height={500} />
+        <div className={styles.onAVineOzzieGonzalez1Parent}>
+          <Link href="/">
+            <button className={styles.textWrapper}>Landscape portfolio</button>
+          </Link>
+        </div>
       </div>
-      <main className={styles.mainInner}>
-        <section className={styles.imageGroup}>
-          <img
-            className={styles.imageIcon}
-            alt=""
-            src="/cursivewritingozziegonzalez-1@2x.png"
-          />
-          <button className={styles.textContainer}>
-            <div className={styles.text}>{`Landscape Portfolio `}</div>
-          </button>
-          <FrameComponent />
-        </section>
-      </main>
+      <div className={styles.imageTextWrapper}>
+        <Image className={styles.imageIcon} alt="" src={Vine} width={400} height={300} />
+        <p className={styles.textWrapper}>Wildlife portfolio</p>
+      </div>
+      <div className={styles.imageTextWrapper}>
+        <Image className={styles.imageIcon} alt="" src={Spot} width={400} height={300} />
+        <p className={styles.textWrapper}>Galleries by location</p>
+      </div>
+      <div className={styles.imageTextWrapper}>
+        <Image className={styles.imageIcon} alt="" src={Days} width={400} height={300} />
+        <p className={styles.textWrapper}>Lifestyle and Adventure portfolio</p>
+      </div>
+      {/* //   <div className={styles.textWrapper}>{`Landscape Portfolio `}</div>
+      // </div>
+      // <main className={styles.mainInner}>
+      //   <section className={styles.imageGroup}>
+      //     <img */}
+      {/* //       className={styles.imageIcon}
+      //       alt=""
+      //       src="/cursivewritingozziegonzalez-1@2x.png"
+      //     />
+      //     <button className={styles.textContainer}>
+      //       <div className={styles.text}>{`Landscape Portfolio `}</div>
+      //     </button>
+      //     <FrameComponent />
+      //   </section> */}
+      // {/* </main> */}
     </div>
   );
 };
@@ -42,40 +56,26 @@ const FrameComponent = () => {
   return (
     <div className={styles.frameParent}>
       <div className={styles.frameGroup}>
-        <div className={styles.onAVineOzzieGonzalez1Parent}>
-          <img
-            className={styles.onAVineOzzieGonzalez1Icon}
-            alt=""
-            src="/onavineozziegonzalez-1@2x.png"
-          />
-          <button className={styles.wildlifePortfolioWrapper}>
-            <div className={styles.wildlifePortfolio}>Wildlife portfolio</div>
-          </button>
-        </div>
-        <div className={styles.theSpotOzzieGonzalez1Parent}>
-          <img
-            className={styles.theSpotOzzieGonzalez1Icon}
-            alt=""
-            src="/thespotozziegonzalez-1@2x.png"
-          />
-          <button className={styles.galleriesByLocationWrapper}>
-            <div className={styles.galleriesByLocation}>
-              Galleries by location
-            </div>
-          </button>
-        </div>
-      </div>
-      <div className={styles.daysEndOzzieGonzalez1Parent}>
-        <img
-          className={styles.daysEndOzzieGonzalez1Icon}
-          alt=""
-          src="/daysendozziegonzalez-1@2x.png"
-        />
-        <div className={styles.lifestyleAndAdventurePortfoWrapper}>
-          <div className={styles.lifestyleAndAdventure}>
-            Lifestyle and adventure portfollio
+        <div>
+          <div className={styles.onAVineOzzieGonzalez1Parent}>
+            <img className={styles.onAVineOzzieGonzalez1Icon} alt="" src="" />
+            <button className={styles.wildlifePortfolioWrapper}>
+              <div className={styles.wildlifePortfolio}>Wildlife portfolio</div>
+            </button>
+          </div>
+          <div className={styles.theSpotOzzieGonzalez1Parent}>
+            <img className={styles.theSpotOzzieGonzalez1Icon} alt="" src="" />
+            <button className={styles.galleriesByLocationWrapper}>
+              <div className={styles.galleriesByLocation}>Galleries by location</div>
+            </button>
           </div>
         </div>
+        {/* <div className={styles.daysEndOzzieGonzalez1Parent}>
+          <img className={styles.daysEndOzzieGonzalez1Icon} alt="" src="" />
+          <div className={styles.lifestyleAndAdventurePortfoWrapper}>
+            <div className={styles.lifestyleAndAdventure}>Lifestyle and adventure portfollio</div>
+          </div>
+        </div> */}
       </div>
     </div>
   );
