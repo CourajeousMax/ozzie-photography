78b7a2e9a92a1cee103bbf0a6ef0520db2812cd7
Stephany Munoz <stephany.munoz8@gmail.com>
2024-10-29 11:26:56 -0400

    fixed config/cloudinary cloudname

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,16 +1,12 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-if (
-  !process.env.CLOUDINARY_CLOUD_NAME ||
-  !process.env.CLOUDINARY_API_KEY ||
-  !process.env.CLOUDINARY_API_SECRET
-) {
+if (!process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
   console.error("Cloudinary configuration is missing.");
 } else {
   console.log("Cloudinary is configured successfully.");
